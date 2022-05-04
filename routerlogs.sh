1f2816b2a927b41c135ec5b867248146c72fbf99
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-05-05 10:15:31 +1200

    Update routerlogs.sh

--- routerlogs.sh
+++ routerlogs.sh
@@ -1,18 +1,11 @@
 rm /tmp/mnt/New_Volume/sort.txt
-/jffs/scripts/WebHistory_Report.sh >> /tmp/mnt/New_Volume/sort.txt
-sed 's/192.168.1.20/Zeppy/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/3C:E0:72:EE:54:81/Zeppy/' /tmp/mnt/New_Volume/sort.txt --in-place
+bash /jffs/scripts/WebHistory_Report.sh >> /tmp/mnt/New_Volume/sort.txt
+sed 's/192.168.1.20/AidanPersonal/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.50/AidanWork/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.26/RPGesus/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/A0:51:0B:CF:F0:DF/RPGesus/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.44/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/24:4B:FE:97:28:4F/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/192.168.1.128/ML01/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/02:0F:B5:A3:DE:55/ML01/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.128/StitchWork/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.83/Extender/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/02:0F:B5:B0:2D:48/Extender/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.98/GalaxyS9/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/02:D5:FB:FA:19:A1/GalaxyS9/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.6/Chromecast/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/1A:24:CE:05:AB:E6/Chromecast/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.84/OPPOA52020/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/F4:D6:20:83:D4:C0/OPPOA52020/' /tmp/mnt/New_Volume/sort.txt --in-place
