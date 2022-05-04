93b087c6d087e151e6ff1cfd998c289eb50bb7c6
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-05-05 09:42:23 +1200

    Update routerlogs.sh

--- routerlogs.sh
+++ routerlogs.sh
@@ -7,3 +7,11 @@ sed 's/192.168.1.44/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/24:4B:FE:97:28:4F/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.128/ML01/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/02:0F:B5:A3:DE:55/ML01/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.83/Extender/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/02:0F:B5:B0:2D:48/Extender/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.98/GalaxyS9/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/02:D5:FB:FA:19:A1/GalaxyS9/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.6/Chromecast/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/1A:24:CE:05:AB:E6/Chromecast/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.84/OPPOA52020/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/F4:D6:20:83:D4:C0/OPPOA52020/' /tmp/mnt/New_Volume/sort.txt --in-place
