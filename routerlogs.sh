409bfd579de4b25b485fb5041ceeaa1a9cc7a782
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-05-05 09:38:16 +1200

    Update routerlogs.sh

--- routerlogs.sh
+++ routerlogs.sh
@@ -6,4 +6,4 @@ sed 's/A0:51:0B:CF:F0:DF/RPGesus/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.44/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/24:4B:FE:97:28:4F/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.128/ML01/' /tmp/mnt/New_Volume/sort.txt --in-place
-sed 's/02:0F:B5:A3:DE:55/RobotPC/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/02:0F:B5:A3:DE:55/ML01/' /tmp/mnt/New_Volume/sort.txt --in-place
