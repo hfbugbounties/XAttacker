8c6d01b9add017e11223c6103d43cb4806c10c1b
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-05-05 09:55:23 +1200

    Update routerlogs.sh

--- routerlogs.sh
+++ routerlogs.sh
@@ -1,5 +1,5 @@
 rm /tmp/mnt/New_Volume/sort.txt
-sqlite3 /jffs/.sys/WebHistory/WebHistory.db "attach '/jffs/.sys/WebHistory/WebHistory.db' as asus;attach '/jffs/.sys/WebHistory/Addons.db' as killeriq;select datetime(timestamp, 'unixepoch', 'localtime') AS time, x.hostname, x.ip, h.mac, h.url from asus.history h join killeriq.hosts x on h.mac = x.mac;" | sort >> /tmp/mnt/New_Volume/sort.txt
+/jffs/scripts/WebHistory_Report.sh >> /tmp/mnt/New_Volume/sort.txt
 sed 's/192.168.1.20/Zeppy/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/3C:E0:72:EE:54:81/Zeppy/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.26/RPGesus/' /tmp/mnt/New_Volume/sort.txt --in-place
