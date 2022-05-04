a1de1b4b70ad97ce06779b4aa48484b610dd0718
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-05-05 11:22:58 +1200

    Update routerlogs2.sh

--- routerlogs2.sh
+++ routerlogs2.sh
@@ -9,5 +9,8 @@ sed 's/192.168.1.83/Extender/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.98/GalaxyS9/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.6/Chromecast/' /tmp/mnt/New_Volume/sort.txt --in-place
 sed 's/192.168.1.84/OPPOA52020/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/192.168.1.116/NintendoSwitch/' /tmp/mnt/New_Volume/sort.txt --in-place
+sed 's/?                 / /' /tmp/mnt/New_Volume/sort.txt --in-place
 sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" /mnt/New_Volume/sort.txt --in-place
+sed -e 's/^[ \t]*//' /mnt/New_Volume/sort.txt --in-place
 sed -i '1,8d' /mnt/New_Volume/sort.txt
